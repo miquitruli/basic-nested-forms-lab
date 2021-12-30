3dc781572796c4f48c9dbb8fa66109d582447f5d
Micaella Colombo <micaelc@g.clemson.edu>
2021-12-30 14:10:21 -0500

    commit tree 73dc8de3b14d1d34ad5a9cf1abc3a96511353a93

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,31 @@
 class RecipesController < ApplicationController
-  def show
-    @recipe = Recipe.find(params[:id])
+  def new
+    @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+  end
+
+  def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
   end
 
   def index
     @recipes = Recipe.all
   end
 
-  def new
-    @recipe = Recipe.new
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 
-  def create
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ])
   end
 end
